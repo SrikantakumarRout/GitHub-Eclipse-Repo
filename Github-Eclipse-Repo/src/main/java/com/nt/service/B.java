2a13e91ea250faeb1cdf496682da7f88c18d97a6
Srikanta kumar Rout <srout9333@gmail.com>
2024-05-02 09:49:49 +0530

    dev1

--- Github-Eclipse-Repo/src/main/java/com/nt/service/B.java
+++ Github-Eclipse-Repo/src/main/java/com/nt/service/B.java
@@ -2,10 +2,8 @@ package com.nt.service;
 
 public class B {
 public void b() {
-<<<<<<< HEAD
-	System.out.println("srikanta");
-=======
-	System.out.println("skr");
->>>>>>> branch 'master' of https://github.com/SrikantakumarRout/GitHub-Eclipse-Repo.git
+
+	System.out.println("srikanta rout");
+
 }
 }
